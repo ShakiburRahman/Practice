f352f693dd9c2b2ac63614602c31093ca406a23c
syed Shakib <syed.shakib75@gmail.com>
2020-03-05 22:49:17 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=2,n=3,p=4;
+		int max=0;
+		
+		if(m>n || m>p ) {
+			max=m;
+	}else  if(p>m || p>n){
+		max=p;}
+		else if(n>m || n>p)
+			max=n;
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		System.out.println(max);
+		
+		
+		System.out.println(max);
+	}
+
+}
